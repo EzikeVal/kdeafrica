57955c9ddec378bc825ec35f6d98c57bfb046e32
ezikechris <christophermbe1009@gmail.com>
2021-03-09 14:44:18 +0100

    Main Commit

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import '../NavBar/NavBar.css';
+class NavBar extends Component {
+  render(){
+
+    return (
+        <div className='NavBar'>
+          <header className='header'> 
+              <ul id="nav">
+                <li><a href="">Products</a></li>
+                <li><a href="">Develop</a></li>
+                <li><a href="">Get Involved</a></li>
+                <li><a href=""></a>Donate</li>
+              </ul>
+              </header>
+            </div>
+            
+    )}
+}
+
+
+
+
+
+
+
+
+export default NavBar;
